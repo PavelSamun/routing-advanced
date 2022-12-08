c0bdfcf4cd9b82a9bc3d2b04f06bed888da58364
Pavel Samun <pashoksamun88@gmail.com>
2022-12-08 15:06:16 +0200

    edit phrases

--- src/app/phrases/phrase-details/phrase-details.component.ts
+++ src/app/phrases/phrase-details/phrase-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Phrase} from "../../shared/phrase";
 import {PhrasesService} from "../../shared/phrases.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../shared/auth.service";
 
 @Component({
   selector: 'app-phrase-details',
@@ -11,10 +12,14 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class PhraseDetailsComponent implements OnInit {
 
   phrase?: Phrase;
+  editValue?: string;
+  editLanguage?: string;
 
   constructor(private phrasesService: PhrasesService,
               private activatedRoute: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              public authService: AuthService
+  ) {
   }
 
   ngOnInit(): void {
@@ -27,14 +32,29 @@ export class PhraseDetailsComponent implements OnInit {
         .getPhrase(id)
         .then(res => {
           this.phrase = res;
+          this.editValue = this.phrase?.value;
+          this.editLanguage = this.phrase?.language;
         });
     });
   }
 
   gotoPhrasesList(): void {
     this.router.navigate(['/phrases', {
-      id: this.phrase?.id
+      id: this.phrase?.id,
+      param1: 'test',
+      param2: 123
     }]).then();
+  }
+
+  save(): void {
+    if (this.phrase) {
+      this.phrase.value = this.editValue || '';
+      this.phrase.language = this.editLanguage || '';
+    }
+  }
+
+  isChanged(): boolean {
+    return !(this.phrase?.value === this.editValue && this.phrase?.language === this.editLanguage)
   }
 
 }
--- src/app/phrases/phrases.module.ts
+++ src/app/phrases/phrases.module.ts
@@ -4,14 +4,16 @@ import {CommonModule} from '@angular/common';
 import {PhrasesRoutingModule} from './phrases-routing.module';
 import {PhrasesListComponent} from "./phrases-list/phrases-list.component";
 import {PhraseDetailsComponent} from "./phrase-details/phrase-details.component";
-import { PhrasesHostComponent } from './phrases-host/phrases-host.component';
+import {PhrasesHostComponent} from './phrases-host/phrases-host.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [PhrasesListComponent, PhraseDetailsComponent, PhrasesHostComponent],
   imports: [
     CommonModule,
-    PhrasesRoutingModule
+    PhrasesRoutingModule,
+    FormsModule
   ]
 })
 export class PhrasesModule {
